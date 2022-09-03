8f80695745f61b0edc365469949378091118cfbc
Vaggelis <vagpappas13@gmail.com>
2022-09-03 16:40:14 +0300

    ticketController

--- src/main/java/groupproject/projectx/controller/TicketController.java
+++ src/main/java/groupproject/projectx/controller/TicketController.java
@@ -1,6 +1,34 @@
 
 package groupproject.projectx.controller;
 
+import groupproject.projectx.model.Flight;
+import groupproject.projectx.model.Ticket;
+import groupproject.projectx.services.TicketService;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/tickets")
 public class TicketController {
     
+     @Autowired
+     private TicketService ticketService;
+    
+     @GetMapping("/alltickets")
+    public ResponseEntity<List<Ticket>> getAllTickets() {
+        List<Ticket> allTickets = new ArrayList();
+        try {
+            allTickets = ticketService.getAllTickets();
+            return new ResponseEntity<>(allTickets, HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(allTickets, HttpStatus.BAD_REQUEST);
+        }
+}
 }
--- src/main/java/groupproject/projectx/services/TicketService.java
+++ src/main/java/groupproject/projectx/services/TicketService.java
@@ -1,14 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package groupproject.projectx.services;
 
-/**
- *
- * @author Vaggelis
- */
+import groupproject.projectx.model.Flight;
+import groupproject.projectx.model.Ticket;
+import groupproject.projectx.repository.FlightRepository;
+import groupproject.projectx.repository.TicketRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TicketService {
+    
+     @Autowired
+    TicketRepository ticketRepository;
+
+    public List<Ticket> getAllTickets() {
+        return ticketRepository.findAll();
+    }
     
 }
